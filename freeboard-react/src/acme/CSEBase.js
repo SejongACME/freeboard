e118d561f1b0a937c9e0d33f193d4e211a70e45b
Subin <rla0010101@gmail.com>
2023-12-09 17:30:35 +0900

    Feat: 모든 기체, 지역 컨테이너에 존재하는 컨테이너 인스턴스 data로 dashboard 구현

--- freeboard-react/src/acme/CSEBase.js
+++ freeboard-react/src/acme/CSEBase.js
@@ -59,40 +59,57 @@ const CSEBase = () => {
       },
     });
   
-    const [selectedCategory, setSelectedCategory] = useState("CO"); // 초기값
+    const [selectedContainer, setSelectedContainer] = useState("CO");
 
-    // ACME server API 연결 test
-    const handleCategoryChange = (category) => {
-        setSelectedCategory(category);
-        console.log(category); // 기체 종류
-
-        const url = `http://192.168.219.103:65535/cse-in/${category}/강남구/la`;
-        const headers = {
-            "X-M2M-Origin": "CAdmin",
-            "X-M2M-RI": "nh35k4rdrt",
-            "X-M2M-RVI": "3",
-        };
-
-        const requestOptions = {
-            method: "GET",
-            headers: headers,
-        };
-
-        fetch(url, requestOptions)
-            .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-            })
-            .then((res) => {
-            console.log("container instance data:", res["m2m:cin"].con); // 기체 농도에 해당하는 con 
-            })
-            .catch((error) => {
-            console.error("API 요청 중 오류 발생:", error);
-            });
+    const handleContainerChange = (gasContainer) => {
+        setSelectedContainer(gasContainer);
+        console.log(gasContainer);  
+    
+        const locationsContainer = ["강남구", "광진구", "성동구", "송파구", "중구"];
         
+        const updatedData = {};
+    
+        locationsContainer.forEach((locationContainer) => {
+            const url = `http://192.168.219.103:65535/cse-in/${gasContainer}/${locationContainer}/la`;
+            console.log(url);
+            const headers = {
+                "X-M2M-Origin": "CAdmin",
+                "X-M2M-RI": "nh35k4rdrt",
+                "X-M2M-RVI": "3",
+            };
+    
+            const requestOptions = {
+                method: "GET",
+                headers: headers,
+            };
+    
+            fetch(url, requestOptions)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((res) => {
+                    const updatedValue = res["m2m:cin"].con;
+                    console.log(gasContainer, locationContainer, updatedValue);
+   
+                    if (!updatedData[gasContainer]) {
+                        updatedData[gasContainer] = {};
+                    }
+                    updatedData[gasContainer][locationContainer] = updatedValue;
+    
+                    setData((prevData) => ({
+                        ...prevData,
+                        ...updatedData,
+                    }));
+                })
+                .catch((error) => {
+                    console.error("API 요청 중 오류 발생:", error);
+                });
+        });
     };
+
 
     
   
@@ -102,26 +119,26 @@ const CSEBase = () => {
           CSEBase
         </Typography>
         <div style={{ display: "flex", marginBottom: "20px" }}>
-          {Object.keys(data).map((category) => (
+          {Object.keys(data).map((Container) => (
             <Button
-              key={category}
-              onClick={() => handleCategoryChange(category)}
-              variant={selectedCategory === category ? "contained" : "outlined"}
+              key={Container}
+              onClick={() => handleContainerChange(Container)}
+              variant={selectedContainer === Container ? "contained" : "outlined"}
               style={{ marginRight: "10px" }}
             >
-              {category}
+              {Container}
             </Button>
           ))}
         </div>
         <Paper elevation={3} style={{ padding: "20px" }}>
           <Typography variant="h5" gutterBottom>
-            서울특별시 지역별 {selectedCategory} 농도
+            서울특별시 지역별 {selectedContainer} 농도
           </Typography>
           <div>
-            {Object.keys(data[selectedCategory]).map((location) => (
+            {Object.keys(data[selectedContainer]).map((location) => (
               <div key={location}>
                 <Typography variant="subtitle1">
-                  {location}: {data[selectedCategory][location]}
+                  {location}: {data[selectedContainer][location]}
                 </Typography>
               </div>
             ))}
